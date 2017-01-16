5e916551f432705a7dd2f255f6c35c8f1726bbdf
cleiton rodrigues <cleiton rodrigues@cleiton>
2017-01-16 14:48:25 -0200

    implementado camada de aplicacao

---
+++ Documentos/codigo fonte/src/br/edu/ifnmg/todeferias/Aplicacao/usuario.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.todeferias.Aplicacao;
+import java.util.Date;
+
+/**
+ *
+ * @author fagner
+ */
+public class usuario implements entidade {
+    private int id;
+    private String nome;
+    private String email;
+    private Date dataNasc;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email
+     * @throws br.edu.ifnmg.todeferias.Aplicacao.ErroValidacao
+     */
+    public void setEmail(String email) throws ErroValidacao {
+        this.email=email;
+    }
+
+    /**
+     * @return the dataNasc
+     */
+    public Date getDataNasc() {
+        return dataNasc;
+    }
+
+    /**
+     * @param dataNasc the dataNasc to set
+     */
+    public void setDataNasc(Date dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+}
+
+
---
+++ Documentos/codigo fonte/src/br/edu/ifnmg/todeferias/Apresentacao/Apresentacao.java
@@ -0,0 +1,22 @@
+package br.edu.ifnmg.todeferias.Apresentacao;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author fagner
+ */
+public class Apresentacao {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
