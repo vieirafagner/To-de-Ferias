ad141fc6cc943a1c98cb1916c597870cce74ecc9
tavaresmanoel <omanoeltavares@gmail.com>
2017-02-07 16:42:12 -0200

    implementando classe Serie.

---
+++ TodeFerias/to_de_ferias_Aplicacao/src/br/edu/ifnmg/todeferias/Aplicacao/Serie.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.todeferias.Aplicacao;
+
+/**
+ *
+ * @author fagner
+ */
+public class Serie implements Entidade {
+    
+        private int id;
+        private int duracao_ep;
+        private String genero;
+        private String nome;
+        private int classificacao;
+        private String sinopse;
+        private int qtd_temp;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the duracao_ep
+     */
+    public int getDuracao_ep() {
+        return duracao_ep;
+    }
+
+    /**
+     * @param duracao_ep the duracao_ep to set
+     */
+    public void setDuracao_ep(int duracao_ep) {
+        this.duracao_ep = duracao_ep;
+    }
+
+    /**
+     * @return the genero
+     */
+    public String getGenero() {
+        return genero;
+    }
+
+    /**
+     * @param genero the genero to set
+     */
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     * @throws br.edu.ifnmg.todeferias.Aplicacao.ErroValidacao
+     */
+    public void setNome(String nome) throws ErroValidacao {
+        if(nome.length() > 250)
+            throw new ErroValidacao("O atributo nome deve ter no máximo 250 caracteres!");
+        this.nome = nome;
+    }
+
+    /**
+     * @return the classificacao
+     */
+    public int getClassificacao() {
+        return classificacao;
+    }
+
+    /**
+     * @param classificacao the classificacao to set
+     */
+    public void setClassificacao(int classificacao) {
+        this.classificacao = classificacao;
+    }
+
+    /**
+     * @return the sinopse
+     */
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    /**
+     * @param sinopse the sinopse to set
+     */
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    /**
+     * @return the qtd_temp
+     */
+    public int getQtd_temp() {
+        return qtd_temp;
+    }
+
+    /**
+     * @param qtd_temp the qtd_temp to set
+     */
+    public void setQtd_temp(int qtd_temp) {
+        this.qtd_temp = qtd_temp;
+    }
+}
--- TodeFerias/to_de_ferias_Persistencia/src/br/edu/ifnmg/todeferias/Persistencia/SerieDAO.java
+++ TodeFerias/to_de_ferias_Persistencia/src/br/edu/ifnmg/todeferias/Persistencia/SerieDAO.java
@@ -5,9 +5,9 @@
  */
 package br.edu.ifnmg.todeferias.Persistencia;
 
-import br.edu.ifnmg.todeferias.Aplicacao.Filme;
 import br.edu.ifnmg.todeferias.Aplicacao.ErroValidacao;
-import br.edu.ifnmg.todeferias.Aplicacao.FilmeRepositorio;
+import br.edu.ifnmg.todeferias.Aplicacao.Serie;
+import br.edu.ifnmg.todeferias.Aplicacao.SerieRepositorio;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -18,32 +18,32 @@ import java.util.logging.Logger;
  *
  * @author fagner
  */
-public class FilmeDAO  extends DAOGenerico<Filme> implements FilmeRepositorio {
+public class SerieDAO  extends DAOGenerico<Serie> implements SerieRepositorio {
         
-    public FilmeDAO() {
-        setConsultaAbrir("select id,diretor,duracao,nome,classificacao,genero,sinopse from Filme where id = ?");
-        setConsultaApagar("DELETE FROM Filme WHERE id = ? ");
-        setConsultaInserir("INSERT INTO Filme(id,diretor,duracao,nome,classificacao,genero,sinopse) VALUES(?,?,?,?,?,?,?)");
-        setConsultaAlterar("UPDATE Filme SET nome = ?, "
+    public SerieDAO() {
+        setConsultaAbrir("select id,duracao_ep,genero,nome,classificacao,sinopse,qtd_temp from Serie where id = ?");
+        setConsultaApagar("DELETE FROM Serie WHERE id = ? ");
+        setConsultaInserir("INSERT INTO Serie(id,duracao_ep,genero,nome,classificacao,sinopse,qtd_temp) VALUES(?,?,?,?,?,?,?)");
+        setConsultaAlterar("UPDATE Serie SET nome = ?, "
                         + "sinopse = ?, genero = ? "
                         + "WHERE id = ?");
-        setConsultaBusca("select id,diretor,duracao,nome,classificacao,genero,sinopse, from Filme ");
-        setConsultaUltimoId("select max(id) from Filme where diretor = ? and duracao = ? and nome = ? and classificacao = ? and genero = ? and sinopse = ?");
+        setConsultaBusca("select id,duracao_ep,genero,nome,classificacao,sinopse,qtd_temp, from Serie ");
+        setConsultaUltimoId("select max(id) from Serie where duracao_ep = ? and genero = ? and nome = ? and classificacao = ? and sinopse = ? and qtd_temp");
     }
     
     @Override
-    protected Filme preencheObjeto(ResultSet resultado) {
+    protected Serie preencheObjeto(ResultSet resultado) {
         // Posso os dados do resultado para o objeto
-                Filme tmp = new Filme();
+                Serie tmp = new Serie();
         try {
             tmp.setId(resultado.getInt(1));
         
-                tmp.setDiretor(resultado.getString(2));
-                tmp.setDuracao(resultado.getInt(3));
+                tmp.setDuracao_ep(resultado.getInt(2));
+                tmp.setGenero(resultado.getString(3));
                 tmp.setNome(resultado.getString(4));
                 tmp.setClassificacao(resultado.getInt(5));
-                tmp.setGenero(resultado.getString(6));
-                tmp.setSinopse(resultado.getString(7));
+                tmp.setSinopse(resultado.getString(6));
+                tmp.setQtd_temp(resultado.getInt(7));
                 
          } catch (SQLException ex) {
             Logger.getLogger(ContaDAO.class.getName()).log(Level.SEVERE, null, ex);
@@ -54,16 +54,15 @@ public class FilmeDAO  extends DAOGenerico<Filme> implements FilmeRepositorio {
                 return tmp;
     }
     
-    @Override
-    protected void preencheConsulta(PreparedStatement sql, Filme obj) {
+    protected void preencheConsulta(PreparedStatement sql, Serie obj) {
         try {
             // Passa os parâmetros para a consulta SQL
-            sql.setString(1, obj.getDiretor());
-            sql.setInt(2, obj.getDuracao());
+            sql.setInt(1, obj.getDuracao_ep());
+            sql.setString(2, obj.getGenero());
             sql.setString(3, obj.getNome());
             sql.setInt(4, obj.getClassificacao());
-            sql.setString(5, obj.getGenero());
-            sql.setString(6, obj.getSinopse());
+            sql.setString(5, obj.getSinopse());
+            sql.setInt(6, obj.getQtd_temp());
             
             if(obj.getId() > 0) sql.setInt(4,obj.getId());
             
@@ -78,12 +77,12 @@ public class FilmeDAO  extends DAOGenerico<Filme> implements FilmeRepositorio {
      * @return
      */
     @Override
-    public Filme Abrir(String nome) {
+    public Serie Abrir(String nome) {
         try {
             
             // Crio a consulta sql
-            PreparedStatement sql = conn.prepareStatement("select id,diretor,duracao,nome,classificacao,genero,sinopse "
-                    + "from Filme where nome = ?");
+            PreparedStatement sql = conn.prepareStatement("select id,duracao_ep,genero,nome,classificacao,sinopse,qtd_temp "
+                    + "from Serie where nome = ?");
             
             // Passo os parâmentros para a consulta sql
             sql.setString(1, nome);
@@ -104,36 +103,36 @@ public class FilmeDAO  extends DAOGenerico<Filme> implements FilmeRepositorio {
     }
     
     @Override
-    protected void preencheFiltros(Filme filtro) {
+    protected void preencheFiltros(Serie filtro) {
         if(filtro.getId() > 0) adicionarFiltro("id", "=");
-        if(filtro.getDiretor()!= null) adicionarFiltro("diretor", " like ");
-        if(filtro.getDuracao()> 0) adicionarFiltro("duracao", "=");
+        if(filtro.getDuracao_ep() > 0) adicionarFiltro("duracao_ep", "=");
+        if(filtro.getGenero() != null) adicionarFiltro("genero", " like ");
         if(filtro.getNome() != null) adicionarFiltro("nome", " like ");
         if(filtro.getClassificacao() > 0) adicionarFiltro("classificacao", "=");
-        if(filtro.getGenero() != null) adicionarFiltro("genero", " like ");
         if(filtro.getSinopse()!= null) adicionarFiltro("sinopse", " like ");
-    
+        if(filtro.getQtd_temp() > 0) adicionarFiltro("qtd_temp", "=");
     }
 
     @Override
-    protected void preencheParametros(PreparedStatement sql, Filme filtro) {
+    protected void preencheParametros(PreparedStatement sql, Serie filtro) {
         try {
             int cont = 1;
             if(filtro.getId() > 0){ sql.setInt(cont, filtro.getId()); cont++; }
-            if(filtro.getDiretor() != null ){ sql.setString(cont, filtro.getDiretor()); cont++; }
-            if(filtro.getDuracao()> 0 ){ sql.setInt(cont, filtro.getDuracao()); cont++; }
+            if(filtro.getDuracao_ep() > 0 ){ sql.setInt(cont, filtro.getDuracao_ep()); cont++; }
+            if(filtro.getGenero()!= null ){ sql.setString(cont, filtro.getGenero()); cont++; }
             if(filtro.getNome() != null ){ sql.setString(cont, filtro.getNome()); cont++; }
             if(filtro.getClassificacao() > 0){ sql.setInt(cont, filtro.getClassificacao()); cont++; }
-            if(filtro.getGenero()!= null ){ sql.setString(cont, filtro.getGenero()); cont++; }
             if(filtro.getSinopse()!= null ){ sql.setString(cont, filtro.getSinopse()); cont++; }
+            if(filtro.getQtd_temp() > 0 ){ sql.setInt(cont, filtro.getQtd_temp()); cont++; }
             
             
         
         } catch (SQLException ex) {
-            Logger.getLogger(FilmeDAO.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(SerieDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
 
     
 }
+
